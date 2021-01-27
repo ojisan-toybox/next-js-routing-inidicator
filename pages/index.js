b38e073c39f64e836a989a7dd5e946dac1c56054
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-28 02:36:12 +0900

    loader

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
 import Link from "next/link";
+import Router from "next/router";
+import { useEffect, useState } from "react";
 
 export default () => {
-  return <Link href="about">heavy about</Link>;
+  const [isLoading, setLoadingState] = useState(false);
+  useEffect(() => {
+    Router.events.on("routeChangeStart", (url) => {
+      setLoadingState(true);
+    });
+  }, []);
+  return isLoading ? (
+    <div
+      style={{
+        background: "rgba(0,0,0,0.8)",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 32,
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <marquee>loading</marquee>
+    </div>
+  ) : (
+    <Link href="about">heavy about</Link>
+  );
 };
