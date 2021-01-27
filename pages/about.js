1e54d52544e78a3174c0e44668b38fa105bfc6f1
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-28 01:41:57 +0900

    add timer

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,14 @@
-export default () => {
-  return <Link href="about">about</Link>;
+export default (props) => {
+  return <div>this is about page: {props.id}</div>;
 };
+
+export async function getServerSideProps(context) {
+  await sleepByPromise(4);
+  return {
+    props: { id: 1 }, // will be passed to the page component as props
+  };
+}
+
+function sleepByPromise(sec) {
+  return new Promise((resolve) => setTimeout(resolve, sec * 1000));
+}
